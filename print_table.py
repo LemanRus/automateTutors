0f319ac0cbc07411ef266c57a5318b0617b9dbc6
LemanRuss <wlad163@gmail.com>
2021-03-04 08:50:04 +0400

    test

--- print_table.py
+++ print_table.py
@@ -6,9 +6,13 @@ table_data = [["apples", "oranges", "cherries", "banana"],
 def print_table(table):
     col_width = [0] * len(table)
     for i in range(len(table_data)):
-        col_width[i] = max(len(table_data[i]))
+        col_width[i] = max(len(element) for element in table_data[i])
 
-    print(col_width)
+    adjust = max(col_width)
+    for i in range(len(table_data[0])):
+        for j in range(len(table_data)):
+            print(table_data[j][i].rjust(adjust + 1), end="")
+        print()
 
 
 print_table(table_data)
