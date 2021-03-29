bf92a9b2732b8f4091813df18a2eb3e40c9be76f
LemanRuss <wlad163@gmail.com>
2021-03-29 08:48:31 +0400

    test

--- regex_strip.py
+++ regex_strip.py
@@ -4,11 +4,16 @@ import re
 def regex_strip(*strings):
     strip_start = re.compile(r"^\s*")
     strip_end = re.compile(r"\s*$")
+    stripped_string = ""
     if len(strings) == 1:
-        return strip_start.sub(st)
+        stripped_string = strip_start.sub("", strings[0])
+        stripped_string = strip_end.sub("", stripped_string)
+        return stripped_string
     else:
-        return strip_end.sub(strings[1], strings[0])
+        stripped_string = strip_start.sub(strings[1], strings[0])
+        stripped_string = strip_end.sub(strings[1], stripped_string)
+        return stripped_string
 
 
-print(regex_strip(" jlkhjlkh "))
-print(regex_strip(" ggjhgjhghg ", "SPAM"))
+print(regex_strip(" jlkhjlkh      "))
+print(regex_strip("       ggjhgjhghg      ", "SPAM"))
--- test.py
+++ test.py
@@ -1,8 +1,2 @@
-def func(n):
-    a=0
-    for i in range(1,n):
-        a = a+i
-        print(a, end=' ')
-
-f = func
-print(f(0))
+import os
+os.path.relpath()
